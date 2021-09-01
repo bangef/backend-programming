0bdd7dbb83f17f6570c2829d06bee04904ff05d8
Aufa Billah <aufaroot18@gmail.com>
2021-09-01 15:14:51 +0700

    create array data user

---
+++ data/user.js
@@ -0,0 +1,25 @@
+const users = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    email: "Sincere@april.biz",
+    address: "Gwenborough",
+    phone: "8031",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    email: "Shanna@melissa.tv",
+    address: "Wisokyburgh",
+    phone: "6593",
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    email: "Nathan@yesenia.net",
+    address: "McKenziehaven",
+    phone: "4447",
+  },
+];
+
+module.exports = { users };
