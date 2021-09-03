86c424e960d7afee35e598646fc3d7f93235ab24
Aufa Billah <aufaroot18@gmail.com>
2021-09-03 15:19:32 +0700

    add new user

---
+++ data/user.js
@@ -0,0 +1,19 @@
+const users = [
+  {
+    id: 1,
+    name: "aufa",
+    address: "depok",
+  },
+  {
+    id: 2,
+    name: "billah",
+    address: "jakarta",
+  },
+  {
+    id: 3,
+    name: "john",
+    address: "bandung",
+  },
+];
+
+module.exports = users;
