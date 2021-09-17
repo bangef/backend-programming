9a212c491fa1e118dc731a8a6557b2cdc65b5389
Aufa Billah <aufaroot18@gmail.com>
2021-09-17 15:06:48 +0700

    Split code to model and controller

--- server.js
+++ server.js
@@ -1,107 +1,29 @@
 const http = require("http");
-const { nanoid } = require("nanoid");
-const users = require("./data/user");
+const UserController = require("./controllers/UserController");
 
 const port = 3000;
-
-const getRequestData = (req) => {
-  return new Promise((resolve) => {
-    let data = "";
-
-    req.on("data", (chunk) => {
-      data += chunk;
-    });
-
-    req.on("end", () => {
-      resolve(JSON.parse(data));
-    });
-  });
-};
 
 const requestListener = async (req, res) => {
   const { method, url } = req;
 
   if (url === "/" && method === "GET") {
     res.end("user api");
-  }
-
-  // jika url /user dan method get
-  else if (url === "/user" && method === "GET") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    const data = JSON.stringify({ users });
-    res.end(data);
-  }
-
-  // jika url /user dan id 1, dan method get
-  else if (url.match(/\/user\/\d/) && method === "GET") {
+  } else if (url === "/users" && method === "GET") {
+    UserController.index(req, res);
+  } else if (url.match(/\/users\/\d/) && method === "GET") {
     const id = url.split("/")[2];
-    const user = users.find((user) => user.id == id);
-
-    if (user) {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      const data = JSON.stringify(user);
-      res.end(data);
-    } else {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      const data = JSON.stringify({
-        status: "failed",
-      });
-      res.end(data);
-    }
-  }
-
-  // jika url /user, dan method post
-  else if (url === "/user" && method === "POST") {
-    const payloads = await getRequestData(req);
-    const newUser = { ...payloads, id: nanoid() };
-    users.push(newUser);
-    const data = JSON.stringify({ users });
-
-    res.writeHead(201, { "Content-Type": "application/json" });
-    res.end(data);
-  }
-
-  // jika url /user/1, dan method put
-  else if (url.match(/\/user\/\d/) && method === "PUT") {
+    UserController.show(req, res, id);
+  } else if (url === "/users" && method === "POST") {
+    UserController.store(req, res);
+  } else if (url.match(/\/users\/\d/) && method === "PUT") {
     const id = url.split("/")[2];
-    const user = users.find((user) => user.id == id);
 
-    if (user) {
-      const payloads = await getRequestData(req);
-      const userIndex = users.findIndex((user) => user.id == id);
-      users[userIndex] = { ...user, ...payloads };
-
-      res.writeHead(200, { "Content-Type": "application/json" });
-      const data = JSON.stringify({ users });
-      res.end(data);
-    } else {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      const data = JSON.stringify({ status: "failed" });
-      res.end(data);
-    }
-  }
-
-  // jika url /user/1, dan method delete
-  else if (url.match(/\/user\/\d/) && method === "DELETE") {
+    UserController.update(req, res, id);
+  } else if (url.match(/\/users\/\d/) && method === "DELETE") {
     const id = url.split("/")[2];
-    const user = users.find((user) => user.id == id);
 
-    if (user) {
-      const userFiltered = users.filter((user) => user.id != id);
-      const data = JSON.stringify(userFiltered);
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(data);
-    } else {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      const data = JSON.stringify({
-        status: "failed",
-      });
-      res.end(data);
-    }
-  }
-
-  // selain itu tampilkan not found
-  else {
+    UserController.destroy(req, res, id);
+  } else {
     const data = JSON.stringify({ status: "failed" });
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(data);
