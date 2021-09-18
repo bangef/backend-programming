66924db613721e4cd9c1602ae19bee98f455b24f
Aufa Billah <aufaroot18@gmail.com>
2021-09-18 20:28:02 +0700

    Modify data user

--- data/user.js
+++ data/user.js
@@ -1,19 +1,25 @@
 const users = [
   {
     id: 1,
-    name: "aufa",
-    address: "depok",
+    name: "Jeremy Cook",
+    email: "jeremy.cook@example.com",
+    address: "2210 E Sandy Lake Rd",
+    phone: "015-0190",
   },
   {
     id: 2,
-    name: "billah",
-    address: "jakarta",
+    name: "Michael Rogers",
+    email: "michael.rogers@example.com",
+    address: "8050 E Little York Rd",
+    phone: "141-3160",
   },
   {
     id: 3,
-    name: "john",
-    address: "bandung",
+    name: "Paula Gordon",
+    email: "paula.gordon@example.com",
+    address: "7871 Robinson Rd",
+    phone: "035-5750",
   },
 ];
 
-module.exports = users;
+module.exports = { users };
