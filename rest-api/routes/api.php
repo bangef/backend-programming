a23288f285e95086e5489a173662669a8eeb0dfb
Aufa Billah <aufaroot18@gmail.com>
2021-10-15 20:02:06 +0700

    Create route student: add get and post method

--- rest-api/routes/api.php
+++ rest-api/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AnimalController;
+# mengimport controller Student
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,14 +21,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-# method get
+# Route animals
 Route::get('/animals', [AnimalController::class, 'index']);
-
-# method post
 Route::post('/animals', [AnimalController::class, 'store']);
-
-# method put
 Route::put('/animals/{id}', [AnimalController::class, 'update']);
-
-# method delete
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+# Method GET, route /students
+Route::get('/students', [StudentController::class, 'index']);
+
+# Method POST, route /students
+Route::post('/students', [StudentController::class, 'store']);
