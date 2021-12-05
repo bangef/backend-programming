b77bcb5a4e655c3acc77e1ab2bfc770bb4373fa4
Aufa Billah <aufaroot18@gmail.com>
2021-12-05 15:51:29 +0700

    Create set-timeout file

---
+++ pertemuan-10/set-timeout.js
@@ -0,0 +1,8 @@
+console.log("Aufa Membuka Bowser Google Chrome.");
+
+setTimeout(function () {
+  console.log("Downloading 1 Hour ...");
+  console.log("Download complete");
+}, 5000);
+
+console.log("Aufa membuka Youtube.");
