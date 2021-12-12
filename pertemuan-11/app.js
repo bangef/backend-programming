e823ecdae1538236f6cc95caef05900965e918b5
Aufa Billah <aufaroot18@gmail.com>
2021-12-12 17:08:16 +0700

    use modular routing (api)

--- pertemuan-11/app.js
+++ pertemuan-11/app.js
@@ -3,16 +3,9 @@ const express = require("express");
 // Membuat object express
 const app = express();
 
-/**
- * Membuat routing.
- * Method get menerima 2 params.
- * Param 1 adalah endpoint.
- * Param 2 callback.
- * Callback menerima object req dan res
- */
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
+// Menggunakan routing (router)
+const router = require("./routes/api.js");
+app.use(router);
 
 // Mendefinisikan port.
 app.listen(3000);
