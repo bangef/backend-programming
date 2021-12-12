ef76e785fda2f7255be52521b286de41cd6126c5
Aufa Billah <aufaroot18@gmail.com>
2021-12-12 21:27:48 +0700

    refactor: add middleware json and urlextended

--- pertemuan-11/app.js
+++ pertemuan-11/app.js
@@ -1,10 +1,15 @@
-// Import express
+// import express dan routing
 const express = require("express");
+const router = require("./routes/api.js");
+
 // Membuat object express
 const app = express();
 
+// Menggunakan middleware
+app.use(express.json());
+app.use(express.urlencoded());
+
 // Menggunakan routing (router)
-const router = require("./routes/api.js");
 app.use(router);
 
 // Mendefinisikan port.
