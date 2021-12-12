5324546581a30bc02493d9e8be57664787683df5
Aufa Billah <aufaroot18@gmail.com>
2021-12-12 21:27:29 +0700

    refactor: update route to use Student Controller in routing student

--- pertemuan-11/routes/api.js
+++ pertemuan-11/routes/api.js
@@ -1,37 +1,17 @@
-// Import express
-const express = require("express");
+// Import Student Controller
+const StudentController = require("../controllers/StudentController");
 
-// Membuat object router
+const express = require("express");
 const router = express.Router();
 
-/**
- * Membuat routing.
- * Method get menerima 2 params.
- * Param 1 adalah endpoint.
- * Param 2 callback.
- * Callback menerima object req dan res
- */
 router.get("/", (req, res) => {
   res.send("Hello Express");
 });
 
-router.get("/students", (req, res) => {
-  res.send("Menampilkan semua students");
-});
-
-router.post("/students", (req, res) => {
-  res.send("Menambahkan data student");
-});
-
-router.put("/students/:id", (req, res) => {
-  const { id } = req.params;
-  res.send(`Mengedit student id ${id}`);
-});
-
-router.delete("/students/:id", (req, res) => {
-  const { id } = req.params;
-  res.send(`Menghapus student id ${id}`);
-});
+router.get("/students", StudentController.index);
+router.post("/students", StudentController.store);
+router.put("/students/:id", StudentController.update);
+router.delete("/students/:id", StudentController.destroy);
 
 // Export router
 module.exports = router;
