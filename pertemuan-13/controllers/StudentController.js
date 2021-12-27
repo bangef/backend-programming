82205b6b369d820e4db9133e15e6a2303a372bfb
Aufa Billah <aufaroot18@gmail.com>
2021-12-27 21:30:35 +0700

    Add update and delete method

--- pertemuan-13/controllers/StudentController.js
+++ pertemuan-13/controllers/StudentController.js
@@ -12,46 +12,59 @@ class StudentController {
       data: students,
     };
 
-    res.json(data);
+    res.status(200).json(data);
   }
 
   async store(req, res) {
-    /**
-     * TODO 2: memanggil method create.
-     * Method create mengembalikan data yang baru diinsert.
-     * Mengembalikan response dalam bentuk json.
-     */
-    // code here
+    const student = await Student.create(req.body);
 
     const data = {
       message: "Menambahkan data student",
-      data: [],
+      data: student,
     };
 
-    res.json(data);
+    res.status(201).json(data);
   }
 
-  update(req, res) {
+  async update(req, res) {
     const { id } = req.params;
-    const { nama } = req.body;
+    const student = await Student.find(id);
 
-    const data = {
-      message: `Mengedit student id ${id}, nama ${nama}`,
-      data: [],
-    };
+    if (student) {
+      const student = await Student.update(id, req.body);
+      const data = {
+        message: `Mengedit data students`,
+        data: student,
+      };
 
-    res.json(data);
+      res.status(200).json(data);
+    } else {
+      const data = {
+        message: `Student not found`,
+      };
+
+      res.status(404).json(data);
+    }
   }
 
-  destroy(req, res) {
+  async destroy(req, res) {
     const { id } = req.params;
+    const student = await Student.find(id);
 
-    const data = {
-      message: `Menghapus student id ${id}`,
-      data: [],
-    };
+    if (student) {
+      await Student.delete(id);
+      const data = {
+        message: `Menghapus data students`,
+      };
 
-    res.json(data);
+      res.status(200).json(data);
+    } else {
+      const data = {
+        message: `Student not found`,
+      };
+
+      res.status(404).json(data);
+    }
   }
 }
 
