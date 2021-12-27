2351b86b58c5c2e8fdb1c0bd410da4604abd210a
Aufa Billah <aufaroot18@gmail.com>
2021-12-27 21:31:04 +0700

    Add models for handle update and delete data

--- pertemuan-13/models/Student.js
+++ pertemuan-13/models/Student.js
@@ -20,13 +20,57 @@ class Student {
     });
   }
 
-  /**
-   * TODO 1: Buat fungsi untuk insert data.
-   * Method menerima parameter data yang akan diinsert.
-   * Method mengembalikan data student yang baru diinsert.
-   */
-  static create() {
-    // code here
+  static async create(data, callback) {
+    // Promise 1: melakukan insert data ke database
+    const id = await new Promise((resolve, reject) => {
+      const sql = "INSERT INTO students SET ?";
+      db.query(sql, data, (err, results) => {
+        resolve(results.insertId);
+      });
+    });
+
+    // Promise 2: melakukan query berdasarkan id
+    return new Promise((resolve, reject) => {
+      const sql = "SELECT * FROM students WHERE id = ?";
+      db.query(sql, id, (err, results) => {
+        resolve(results);
+      });
+    });
+  }
+
+  // mencari data berdasarkan id
+  static find(id) {
+    return new Promise((resolve, reject) => {
+      const sql = "SELECT * FROM students WHERE id = ?";
+      db.query(sql, id, (err, results) => {
+        // destructing array
+        const [student] = results;
+        resolve(student);
+      });
+    });
+  }
+
+  // mengupdate data student
+  static async update(id, data) {
+    await new Promise((resolve, reject) => {
+      const sql = "UPDATE students SET ? WHERE id = ?";
+      db.query(sql, [data, id], (err, results) => {
+        resolve(results);
+      });
+    });
+
+    // mencari data yang baru diupdate
+    const student = await this.find(id);
+    return student;
+  }
+
+  static delete(id) {
+    return new Promise((resolve, reject) => {
+      const sql = "DELETE FROM students WHERE id = ?";
+      db.query(sql, id, (err, results) => {
+        resolve(results);
+      });
+    });
   }
 }
 
